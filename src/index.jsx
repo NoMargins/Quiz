eb02d68d886a0e66f93687ece415cf2f22212f2f
NoMargins <olenaryabenko@gmail.com>
2023-08-23 19:54:35 +0300

    4th

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import store from './store';
 import App from './App';
 import './root.scss';
 
-
 const root = document.getElementById('root');
-const appRoot = ReactDOM.createRoot(root);
 
-appRoot.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  root
 );
