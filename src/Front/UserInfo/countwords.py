d99d137f0304ee46d2481db15af3b58719366a34
NoMargins <olenaryabenko@gmail.com>
2023-08-29 11:59:00 +0300

    ok

--- src/Front/UserInfo/countwords.py
+++ src/Front/UserInfo/countwords.py
@@ -11,7 +11,7 @@ def count_chars_in_non_blue_cells(filename):
     for sheet in wb:
         for row in sheet.iter_rows():
             for cell in row:
-                if cell.fill.start_color.index != "FFc9daf8":  # Синій колір у форматі ARGB
+                if cell.fill.start_color.index = "FFFFFFFF":  # Синій колір у форматі ARGB
                     if cell.value and isinstance(cell.value, str):
                         if ukr_pattern.match(cell.value):
                             total_chars += len(cell.value)
