f4c3aa684459cc260b51324cce249f8a42fd478d
NoMargins <olenaryabenko@gmail.com>
2023-08-29 11:44:57 +0300

    ок

--- src/Front/UserInfo/countwords.py
+++ src/Front/UserInfo/countwords.py
@@ -1,15 +1,20 @@
 import openpyxl
+import re
 
 def count_chars_in_non_blue_cells(filename):
     wb = openpyxl.load_workbook(filename)
     total_chars = 0
 
+    # Регулярний вираз для визначення тексту українською
+    ukr_pattern = re.compile(r"^[А-ЩЬЮЯҐЄІЇа-щьюяґєії\s]+$")
+
     for sheet in wb:
         for row in sheet.iter_rows():
             for cell in row:
-                if cell.fill.start_color.index != "#c9daf8":  # Синій колір у форматі ARGB
+                if cell.fill.start_color.index != "№c9daf8":  # Синій колір у форматі ARGB
                     if cell.value and isinstance(cell.value, str):
-                        total_chars += len(cell.value)
+                        if ukr_pattern.match(cell.value):
+                            total_chars += len(cell.value)
 
     return total_chars
 
